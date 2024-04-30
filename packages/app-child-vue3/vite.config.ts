0648e26d2e9211d2f128dabf6d829aec029a55b7
chenpengxiang <chenpengxiang@dianxiaomi.com>
2024-04-30 11:04:11 +0800

    feat: :sparkles: 修改vite.config,读取环境变量

--- packages/app-child-vue3/vite.config.ts
+++ packages/app-child-vue3/vite.config.ts
@@ -1,7 +1,15 @@
-import { defineConfig } from 'vite'
+import { ConfigEnv, loadEnv, UserConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import { resolve } from 'path'
 
 // https://vitejs.dev/config/
-export default defineConfig({
-    plugins: [vue()],
-})
+export default ({ mode }: ConfigEnv): UserConfig => {
+    const { VITE_CHILD_PORT } = loadEnv(mode, resolve(__dirname, '../..'))
+
+    return {
+        server: {
+            port: Number(VITE_CHILD_PORT),
+        },
+        plugins: [vue()],
+    }
+}
--- packages/app-main-vue3/vite.config.ts
+++ packages/app-main-vue3/vite.config.ts
@@ -1,7 +1,14 @@
-import { defineConfig } from 'vite'
+import { ConfigEnv, loadEnv, UserConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import { resolve } from 'path'
 
 // https://vitejs.dev/config/
-export default defineConfig({
-    plugins: [vue()],
-})
+export default ({ mode }: ConfigEnv): UserConfig => {
+    const { VITE_MAIN_PORT } = loadEnv(mode, resolve(__dirname, '../..'))
+    return {
+        server: {
+            port: Number(VITE_MAIN_PORT),
+        },
+        plugins: [vue()],
+    }
+}
